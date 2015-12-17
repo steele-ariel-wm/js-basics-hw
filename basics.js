37968afc5ce53eb04283e4010c69062de782c989
Ariel <ariel.steele@west-mec.org>
2015-12-17 13:16:10 -0700

    n

--- basics.js
+++ basics.js
@@ -1,26 +1,35 @@
+
 // What is the return value of the below code sample? Provide a sentence or two of explanation.
 typeof( 15 );
+>> "number" The return value is number because it is stating what 15 is, a number.
 
 // What is the return value of the below code sample? Provide a sentence or two of explanation.
 typeof( "hello" );
+>> "string" The return is string because a string has quotes around it and hello had quotes around it.
 
 // What is the return value of the below code sample? Provide a sentence or two of explanation.
 typeof( [ "dog", "cat", "horse" ] );
-
+>> "object" The return is object because there are only two data types and it would usually show what type of array it is but because it is not an array,
+    it has to be an object.
 // What is the return value of the below code sample? Provide a sentence or two of explanation.
 typeof( NaN );
+>> "number" The return was number because Not a Number is equal to zero basically.
 
 // What is the return value of the below code sample? Provide a sentence or two of explanation.
 "hamburger" + "s";
+>> "hamburgers" The return is hamburgers because it's showing hamburger plus s.
 
 // What is the return value of the below code sample? Provide a sentence or two of explanation.
 "hamburgers" - "s";
+>> NaN The return is NaN because hamburgers minus s isn't an actual number / equation.
 
 // What is the return value of the below code sample? Provide a sentence or two of explanation.
 "johnny" + 5;
+>> "johnny5" The return is johnny5 because it's showing johnny plus 5.
 
 // What is the return value of the below code sample? Provide a sentence or two of explanation.
 99 * "luftbaloons";
+>> NaN The return is NaN because luftbaloons isn't a number.
 
 // What will the contents of the below array be after the below code sample is executed.
 var numbers = [ 2, 4, 6, 8 ];
